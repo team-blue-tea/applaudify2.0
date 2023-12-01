32185d3e70db1d2b492014e6ee0306ae491c0265
ica1130 <ilijakrilovic@gmail.com>
2023-12-01 09:54:08 +0100

    refactor Member model
    refactored Member model to use Lombok annotations.

--- server/src/main/java/com/example/server/model/Member.java
+++ server/src/main/java/com/example/server/model/Member.java
@@ -1,6 +1,9 @@
 package com.example.server.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.util.Date;
@@ -8,6 +11,9 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "member")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
     @Id
@@ -16,13 +22,9 @@ public class Member {
     private UUID id;
 
     private String email;
-
     private String name;
-
     private String jobTitle;
-
     private String company;
-
     private String avatarUrl;
 
     @Column(length = 1000)
@@ -37,95 +39,4 @@ public class Member {
     @Temporal(TemporalType.TIMESTAMP)
     @CreationTimestamp
     private Date createdAt;
-
-    public Member() {}
-
-    public Member(UUID id, String email, String name, String jobTitle, String company, String avatarUrl, String bio, String skills, String experience, Date createdAt) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.jobTitle = jobTitle;
-        this.company = company;
-        this.avatarUrl = avatarUrl;
-        this.bio = bio;
-        this.skills = skills;
-        this.experience = experience;
-        this.createdAt = createdAt;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public void setAvatarUrl(String avatarUrl) {
-        this.avatarUrl = avatarUrl;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    public String getSkills() {
-        return skills;
-    }
-
-    public void setSkills(String skills) {
-        this.skills = skills;
-    }
-
-    public String getExperience() {
-        return experience;
-    }
-
-    public void setExperience(String experience) {
-        this.experience = experience;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
 }
